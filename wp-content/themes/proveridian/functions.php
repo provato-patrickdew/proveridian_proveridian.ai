a69d0e419aff7e2af51790fb781f6c423cc0a370
Patrick Dew <74983827+provato-patrickdew@users.noreply.github.com>
2026-08-05 13:53:00 -0400

    remov smooth, standard smooth

--- wp-content/themes/proveridian/functions.php
+++ wp-content/themes/proveridian/functions.php
@@ -149,8 +149,6 @@ function proveridian_scripts() {
 
 	wp_enqueue_script( 'proveridian-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script( 'proveridian-smooth-scroll', get_template_directory_uri() . '/js/smooth-scroll.js', array(), _S_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
